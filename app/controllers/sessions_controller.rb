3c74d7ad60377e8e42d35af0ec5447aae542db9f
Cassandre-Guinut <guinutcassandre@gmail.com>
2020-01-22 13:31:51 +1300

    create logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
       flash[:notice] = "Succesfully logged in"
-      redirect_to users_path(user)
+      redirect_to user_path(user)
     else
       flash.now[:notice] = "There was something wrong with your info"
       render 'new'
@@ -16,7 +16,9 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-
+    session[:user_id] = nil
+    flash[:notice] = "You have logged out"
+    redirect_to root_path
   end
 
 end
