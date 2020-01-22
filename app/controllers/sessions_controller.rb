138623d860a18f5bf01598dd0e50432dad969e22
Cassandre-Guinut <guinutcassandre@gmail.com>
2020-01-22 13:27:00 +1300

    implement log in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,15 @@ class SessionsController < ApplicationController
   end
 
   def create
-    render 'new'
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:notice] = "Succesfully logged in"
+      redirect_to users_path(user)
+    else
+      flash.now[:notice] = "There was something wrong with your info"
+      render 'new'
+    end
   end
 
   def destroy
