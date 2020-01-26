063eb863ae56334db67a732e5f47a5d868be1cf9
Cassandre-Guinut <guinutcassandre@gmail.com>
2020-01-27 10:06:31 +1300

    conditionally render login logout and signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+
+  before_action :set_user, only: [:edit, :update, :show]
   def index
     @users = User.all
   end
@@ -18,15 +20,12 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Your account was updated"
       redirect_to articles_path
@@ -38,5 +37,9 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+  
+  def set_user
+    @user = User.find(params[:id])
   end
 end
